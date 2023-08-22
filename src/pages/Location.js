f4c55a144537468ef5909bd567d78290b38a1aeb
Lukas Mikuta <lukasenokmikuta@gmail.com>
2023-08-22 13:53:13 +0200

    Rettet tilbage, som det var før

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,123 +1,55 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
- 
-
- 
 
 const Location = () => {
-
-    const [myPos, setMyPos] = useState();
-
+    const [ip, setIp] = useState();
     const [dynPos, setDynPos] = useState();
+    const [myPos, setmyPos] = useState(null);
 
-    const [ipAddress, setIpAddress] = useState("");
-
- 
-
-    // Get user's ip adress
+    const getIp = async () => {
+        const response = await fetch("https://ipapi.co/json/");
+        const data = await response.json();
+        setIp(data.ip);
+    };
 
     useEffect(() => {
-
-      const fetchIp = async () => {
-
-        try {
-
-          const response = await fetch("https://api.ipify.org?format=json");
-
-          const data = await response.json();
-
-          setIpAddress(data.ip);
-
-        } catch (error) {
-
-          console.error(error);
-
-        }
-
-      };
-
-      fetchIp();
-
+        getIp();
     }, []);
 
- 
-
- 
-
     useEffect(() => {
-
-        if('geolocation' in navigator) {
-
+        if ("geolocation" in navigator) {
             navigator.geolocation.getCurrentPosition((position) => {
-
-                setMyPos(position)
-
-            })
-
+                console.log(position);
+                setmyPos(position);
+            });
         }
-
     }, []);
-
- 
-
- 
 
     const handleDynamicPosition = (position) => {
-
         setDynPos(position)
-
     }
 
- 
-
-    const handleDynamicPositionError = (err) => {
-
-        console.log(err)
-
-    }
-
- 
-
-    navigator.geolocation.watchPosition(handleDynamicPosition, handleDynamicPositionError, {timeout: 5000})
-
- 
+    navigator.geolocation.watchPosition(handleDynamicPosition, handleDynamicPosition, {timeout: 2000})
 
     return (
-
         <>
+            <p>Din ip adresse er: {ip}.</p>
 
-            <p>Location page</p>
-
- 
-
+            <h1>Locations</h1>
             <p>Latitude: {myPos?.coords.latitude}</p>
-
             <p>Longitude: {myPos?.coords.longitude}</p>
-
+            <p>Altitude: {myPos?.coords.altitude}</p>
             <p>Speed: {myPos?.coords.speed}</p>
-
             <p>Accuracy: {myPos?.coords.accuracy}</p>
 
-            <p>Your ip adress: {ipAddress}</p>
-
- 
-
-            <h1>This is dynamic position</h1>
-
+            <h1>Dynamic position</h1>
             <p>Latitude: {dynPos?.coords.latitude}</p>
-
             <p>Longitude: {dynPos?.coords.longitude}</p>
-
+            <p>Altitude: {dynPos?.coords.altitude}</p>
             <p>Speed: {dynPos?.coords.speed}</p>
-
             <p>Accuracy: {dynPos?.coords.accuracy}</p>
-
         </>
-
-     );
-
-}
-
- 
+    );
+};
 
 export default Location;
