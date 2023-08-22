64b86ccd7d692cdd2ed2cbe696311e5ca26f7a5c
Lukas Mikuta <lukasenokmikuta@gmail.com>
2023-08-22 13:50:37 +0200

    Location.js opdateret, tester om det virker

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,55 +1,123 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
+ 
+
+ 
 
 const Location = () => {
-    const [ip, setIp] = useState();
+
+    const [myPos, setMyPos] = useState();
+
     const [dynPos, setDynPos] = useState();
-    const [myPos, setmyPos] = useState(null);
 
-    const getIp = async () => {
-        const response = await fetch("https://ipapi.co/json/");
-        const data = await response.json();
-        setIp(data.ip);
-    };
+    const [ipAddress, setIpAddress] = useState("");
 
-    useEffect(() => {
-        getIp();
-    }, []);
+ 
+
+    // Get user's ip adress
 
     useEffect(() => {
-        if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                console.log(position);
-                setmyPos(position);
-            });
+
+      const fetchIp = async () => {
+
+        try {
+
+          const response = await fetch("https://api.ipify.org?format=json");
+
+          const data = await response.json();
+
+          setIpAddress(data.ip);
+
+        } catch (error) {
+
+          console.error(error);
+
         }
+
+      };
+
+      fetchIp();
+
     }, []);
+
+ 
+
+ 
+
+    useEffect(() => {
+
+        if('geolocation' in navigator) {
+
+            navigator.geolocation.getCurrentPosition((position) => {
+
+                setMyPos(position)
+
+            })
+
+        }
+
+    }, []);
+
+ 
+
+ 
 
     const handleDynamicPosition = (position) => {
+
         setDynPos(position)
+
     }
 
-    navigator.geolocation.watchPosition(handleDynamicPosition, handleDynamicPosition, {timeout: 5000})
+ 
+
+    const handleDynamicPositionError = (err) => {
+
+        console.log(err)
+
+    }
+
+ 
+
+    navigator.geolocation.watchPosition(handleDynamicPosition, handleDynamicPositionError, {timeout: 5000})
+
+ 
 
     return (
+
         <>
-            <p>Din ip adresse er: {ip}.</p>
 
-            <h1>Locations</h1>
+            <p>Location page</p>
+
+ 
+
             <p>Latitude: {myPos?.coords.latitude}</p>
+
             <p>Longitude: {myPos?.coords.longitude}</p>
-            <p>Altitude: {myPos?.coords.altitude}</p>
+
             <p>Speed: {myPos?.coords.speed}</p>
+
             <p>Accuracy: {myPos?.coords.accuracy}</p>
 
-            <h1>Dynamic position</h1>
-            <p>Latitude: {myPos?.coords.latitude}</p>
-            <p>Longitude: {myPos?.coords.longitude}</p>
-            <p>Altitude: {myPos?.coords.altitude}</p>
-            <p>Speed: {myPos?.coords.speed}</p>
-            <p>Accuracy: {myPos?.coords.accuracy}</p>
+            <p>Your ip adress: {ipAddress}</p>
+
+ 
+
+            <h1>This is dynamic position</h1>
+
+            <p>Latitude: {dynPos?.coords.latitude}</p>
+
+            <p>Longitude: {dynPos?.coords.longitude}</p>
+
+            <p>Speed: {dynPos?.coords.speed}</p>
+
+            <p>Accuracy: {dynPos?.coords.accuracy}</p>
+
         </>
-    );
-};
+
+     );
+
+}
+
+ 
 
 export default Location;
