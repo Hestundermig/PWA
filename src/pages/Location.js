13fb90ef109c345945a6364b5fb5e0551e8e0390
Lukas Mikuta <lukasenokmikuta@gmail.com>
2023-08-22 13:22:00 +0200

    Dynamic location og ip adresse

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,30 +1,55 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+
 
 const Location = () => {
+    const [ip, setIp] = useState();
+    const [dynPos, setDynPos] = useState();
+    const [myPos, setmyPos] = useState(null);
 
-    const [myPos, setmyPos] = useState();
+    const getIp = async () => {
+        const response = await fetch("https://ipapi.co/json/");
+        const data = await response.json();
+        setIp(data.ip);
+    };
+
+    useEffect(() => {
+        getIp();
+    }, []);
 
     useEffect(() => {
         if ("geolocation" in navigator) {
-            /* geolocation is available */
-          } navigator.geolocation.getCurrentPosition((position) => {
-            console.log(position)
-            setmyPos(position)
-          })
+            navigator.geolocation.getCurrentPosition((position) => {
+                console.log(position);
+                setmyPos(position);
+            });
+        }
     }, []);
 
-    return ( 
-        <>
-            <h1>Location page</h1>
+    const handleDynamicPosition = (position) => {
+        setDynPos(position)
+    }
 
+    navigator.geolocation.watchPosition(handleDynamicPosition, handleDynamicPosition, {timeout: 5000})
+
+    return (
+        <>
+            <p>Din ip adresse er: {ip}.</p>
+
+            <h1>Locations</h1>
             <p>Latitude: {myPos?.coords.latitude}</p>
             <p>Longitude: {myPos?.coords.longitude}</p>
             <p>Altitude: {myPos?.coords.altitude}</p>
             <p>Speed: {myPos?.coords.speed}</p>
             <p>Accuracy: {myPos?.coords.accuracy}</p>
-        
+
+            <h1>Dynamic position</h1>
+            <p>Latitude: {myPos?.coords.latitude}</p>
+            <p>Longitude: {myPos?.coords.longitude}</p>
+            <p>Altitude: {myPos?.coords.altitude}</p>
+            <p>Speed: {myPos?.coords.speed}</p>
+            <p>Accuracy: {myPos?.coords.accuracy}</p>
         </>
-     );
-}
- 
+    );
+};
+
 export default Location;
