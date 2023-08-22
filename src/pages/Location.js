f6ccea6fd0e88d864234d50e3940af085e39cb3f
Lukas Mikuta <lukasenokmikuta@gmail.com>
2023-08-22 12:50:58 +0200

    Locationpage commit

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,9 +1,28 @@
-
+import { useState, useEffect } from "react";
 
 const Location = () => {
+
+    const [myPos, setmyPos] = useState();
+
+    useEffect(() => {
+        if ("geolocation" in navigator) {
+            /* geolocation is available */
+          } navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position)
+            setmyPos(position)
+          })
+    }, []);
+
     return ( 
         <>
-            <p>Location page</p>
+            <h1>Location page</h1>
+
+            <p>Latitude: {myPos?.coords.latitude}</p>
+            <p>Longitude: {myPos?.coords.longitude}</p>
+            <p>Altitude: {myPos?.coords.altitude}</p>
+            <p>Speed: {myPos?.coords.speed}</p>
+            <p>Accuracy: {myPos?.coords.accuracy}</p>
+        
         </>
      );
 }
